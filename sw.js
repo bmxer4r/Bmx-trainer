e07f26c85bcab126ea24f511b10550780691cb33
bmxer4r <evertsstan655@gmail.com>
2026-01-04 16:00:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'bmx-sprint-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'bmx-pro-v2';
+const ASSETS = [
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
